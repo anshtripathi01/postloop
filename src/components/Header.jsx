f97e090532b909fb903a383e08e0f937f78f95a2
anshtripathi01 <tripathiansh57@gmail.com>
2025-12-06 20:08:01 +0530

    fixed build error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/authContext";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
